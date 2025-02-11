35a1fbcc535a35b078031ce9f7ce580a3f9fb0c2
Daniel Restrepo <drones9182@gmail.com>
2025-02-10 22:56:31 -0500

    reqs

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print('Hello World')
+import requests
+
+url = "http://localhost:8080/api/v1/file"
+headers = {"Girder-Token": "your_api_token"}
+with open("data.csv", "rb") as file:
+    response = requests.post(url, headers=headers, files={"file": file})
+print(response.json())
