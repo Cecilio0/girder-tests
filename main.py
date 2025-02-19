f22be6be9b4cf07c9da81f9876bbb91bb082f003
Daniel Restrepo <drones9182@gmail.com>
2025-02-18 23:16:54 -0500

    Created first plugin and a simple resource

--- main.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = "http://localhost:8080/api/v1/file"
-headers = {"Girder-Token": "your_api_token"}
-with open("data.csv", "rb") as file:
-    response = requests.post(url, headers=headers, files={"file": file})
-print(response.json())
